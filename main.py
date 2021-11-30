4f843e6d52aa2dedddbe89628e12312fccf5dcf1
simeonwetzel <simeon.wetzel@tu-dresden.de>
2021-11-30 10:13:57 +0100

    First working prototype... implemented external excel metadata aquisition

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ import logging
 import urllib
 
 import geojson
+import text_unidecode
 import yaml
 from dateutil import parser
 from owslib.wcs import WebCoverageService
 from owslib.wfs import WebFeatureService
 from owslib.wms import WebMapService
-from pyproj import CRS, Transformer
-
 from pandas import *
-from math import isnan
+from pyproj import CRS, Transformer
 
 config = yaml.safe_load(open('config.yml'))
 
@@ -26,15 +25,35 @@ def crs_transformation(coords, epsg_old, epsg_new):
     transformer = Transformer.from_crs(old_crs, new_crs)
     transformed_coords = transformer.transform(coords[1], coords[0])
 
-    return transformed_coords[0], transformed_coords[1]
+    return transformed_coords[1], transformed_coords[0]
+
+
+def make_geojson_object_from_bbox(bbox_coords, bbox_crs):
+    """Creating BBOX In EPSG 4326 compliant with CKAN spatial"""
+    # BBOX format ymin, xmin, ymax, xmax'''
+    bbox_ymin = bbox_coords[0]
+    bbox_ymax = bbox_coords[2]
+    bbox_xmin = bbox_coords[1]
+    bbox_xmax = bbox_coords[3]
+
+    spatial = geojson.MultiPolygon([[[
+        crs_transformation([bbox_ymin, bbox_xmin], bbox_crs, 4326),
+        crs_transformation([bbox_ymin, bbox_xmax], bbox_crs, 4326),
+        crs_transformation([bbox_ymax, bbox_xmax], bbox_crs, 4326),
+        crs_transformation([bbox_ymax, bbox_xmin], bbox_crs, 4326),
+        crs_transformation([bbox_ymin, bbox_xmin], bbox_crs, 4326),
+    ]]])
+
+    return spatial
 
 
 def create_metadata_dict_from_xls_file(xls_path):
     xls = ExcelFile(xls_path)
     df = xls.parse(xls.sheet_names[0])
+    xls.close()
 
-    # Remove first two data rows (example dummies)
-    df = df[2:]
+    # Remove first three data rows (example dummies)
+    df = df[3:]
     df = df.where(pandas.notnull(df), None)
     # Creating dict like follows:
     # dict like {column -> {index -> value}}
@@ -50,206 +69,88 @@ def create_metadata_dict_for_dataset(dataset_name, external_metadata_dict):
         idx_in_dict = list(datasets.keys())[list(datasets.values()).index(dataset_name)]
         for key in external_metadata_resource.keys():
             meta_dict[key] = external_metadata_resource[key][idx_in_dict]
+    else:
+        log.info("No external Metadata found for dataset: {}".format(dataset_name))
     return meta_dict
 
 
-def create_dataset_dicts_from_wcs_wms(wms, wcs, metadata_resource_dict):
+def create_dataset_dicts_from_wfs_or_wcs(wms, wfs_or_wcs, metadata_resource_dict, service_type):
     # log.debug(wms.identification.type)
     # log.debug(wms.identification.title)
     # log.debug(wms.identification.abstract)
-
-    wcs_contents = [layer.replace('__', ':') for layer in wcs.contents]  # datastore:layername is needed for wms
-    wms_contents = list(wms.contents)
-
-    log.debug(wcs_contents)
-    log.debug(wms_contents)
+    service = wfs_or_wcs
+    if service_type == 'wcs':
+        """WCS"""
+        service_contents = [layer.replace('__', ':') for layer in
+                            wfs_or_wcs.contents]  # datastore:layername is needed for wms
+    else:
+        """WFS"""
+        service_contents = list(service.contents)
 
     dataset_dicts = []
 
-    # Get all information about layers that support wcs, but use wms to gather information
-    for layer in wcs_contents:
-        cur_wms = wms[layer]
-        gs_datastore = cur_wms.id.split(':')[0]
+    for layer in service_contents:
+        if service_type == 'wcs':
+            cur_service = wms[layer]
+        else:
+            cur_service = service[layer]
 
-
+        gs_datastore = cur_service.id.split(':')[0]
 
         resource_url_wms = config['geoserver']['url'] + gs_datastore + '/wms?request=GetCapabilities'
         resource_url_wcs = config['geoserver']['url'] + gs_datastore + '/wcs?request=GetCapabilities'
-        title = cur_wms.title
-
-        meta_dict = create_metadata_dict_for_dataset(dataset_name=title,
-                                                     external_metadata_dict=external_metadata_resource)
-
-        if meta_dict['alternate_ckan_org'] is not None:
-            owner_org = meta_dict['alternate_ckan_org']
-        else:
-            owner_org = gs_datastore
-
-        # Create Organization with gs_datastore name in ckan if not already exists
-        create_ckan_organization_if_not_exists(owner_org)
-
-
-        abstract = cur_wms.abstract
-        bbox = cur_wms.boundingBox
-        crs = cur_wms.crs_list[1][4].split(':')[1]
-        # log.debug("crs = " + crs)
-
-        '''BBOX format ymin, xmin, ymax, xmax'''
-        bbox_ymin = bbox[0]
-        bbox_ymax = bbox[2]
-        bbox_xmin = bbox[1]
-        bbox_xmax = bbox[3]
-
-        spatial = geojson.MultiPolygon([[[
-            crs_transformation([bbox_ymin, bbox_xmin], crs, 4326),
-            crs_transformation([bbox_ymin, bbox_xmax], crs, 4326),
-            crs_transformation([bbox_ymax, bbox_xmax], crs, 4326),
-            crs_transformation([bbox_ymax, bbox_xmin], crs, 4326),
-            crs_transformation([bbox_ymin, bbox_xmin], crs, 4326),
-        ]]])
-
-        keywords = cur_wms.keywords
-        timepositions = cur_wms.timepositions
-
-        conforms_to = 'http://www.opengis.net/def/crs/EPSG/0/{}'.format(crs)
-        scale_hint = cur_wms.scaleHint
-
-        '''Handle temporal resolution'''
-        if timepositions is not None:
-            timepositions_prep = timepositions[0].split('/')
-            temporal_start = parser.parse(timepositions_prep[0])
-            temporal_end = parser.parse(timepositions_prep[1])
-        else:
-            temporal_start = None
-            temporal_end = None
-
-        organization = wms.provider.contact.organization
-        # contact_name = wms.provider.contact.name
-        contact_name = "Hydro TUD"
-        contact_email = wms.provider.contact.email
-        # dataUrls = cur_wms.dataUrls
-        # metadataUrl = cur_wms.metadataUrls
-
-        # ,dataUrls,
-        # metadataUrl))
-
-        """Package for ckan upload"""
-        resource_dict = [
-            {
-                'url': resource_url_wms,
-                'name': title,
-                'format': 'wms',
-                'licence': None,
-                'description': None,
-            },
-            {
-                'url': resource_url_wcs,
-                'name': title,
-                'format': 'wcs',
-                'licence': None,
-                'description': None,
-            }
-        ]
-        # handle url
-        # Todo make this better
-        ckan_dataset_url = title \
-            .lower() \
-            .replace('(', '') \
-            .replace(')', '') \
-            .strip() \
-            .replace(' ', '-')
-
-        package_dict = {
-            'title': title,
-            'identifier': title,  # Name is mandatory
-            'name': ckan_dataset_url,  # url
-            'notes': abstract,
-            'documentation': None,
-            'contact_name': contact_name,
-            'contact_uri': None,
-            'url': None,
-            'tag_string': 'test',
-            # 'theme': keywords,
-            'theme': 'Boden',  # hardcoded for testing
-            'spatial': str(spatial),
-            'conforms_to': conforms_to,
-            'spatial_resolution': None,
-            'spatial_resolution_type': None,
-            'temporal_start': None,
-            'temporal_end': None,
-            'temporal_resolution': None,
-            'is_version_of': None,
-            'related_resource': None,
-            'derived_from': None,
-            'owner_org': owner_org.lower(),
-            'license_id': None,
-            'resources': resource_dict
-        }
-        dataset_dicts.append(package_dict)
-
-    log.info("Created packages from wcs sources. Ready for upload to ckan")
-    return dataset_dicts
-
-
-def create_dataset_dicts_from_wfs(wfs, metadata_resource):
-    # log.debug(wms.identification.type)
-    # log.debug(wms.identification.title)
-    # log.debug(wms.identification.abstract)
-
-    wfs_contents = list(wfs.contents)
-    log.debug(wfs_contents)
-
-    dataset_dicts = []
-
-    for layer in wfs_contents:
-        cur_wfs = wfs[layer]
-        gs_datastore = cur_wfs.id.split(':')[0]
-
-        resource_url_wms = config['geoserver']['url'] + gs_datastore + '/wms?request=GetCapabilities'
         resource_url_wfs = config['geoserver']['url'] + gs_datastore + '/wfs?request=GetCapabilities'
-        title = cur_wfs.title
+        # title = cur_wfs.title
 
-        meta_dict = create_metadata_dict_for_dataset(dataset_name=title,
-                                                     external_metadata_dict=external_metadata_resource)
-        """
-        if title in external_metadata_resource['dataset_name'].values():
-            datasets = external_metadata_resource['dataset_name']
-            idx_in_dict = list(datasets.keys())[list(datasets.values()).index(title)]
-            for key in external_metadata_resource.keys():
-                meta_dict[key] = external_metadata_resource[key][idx_in_dict]
-        """
+        # Search in Metadata from excel-source and create dict
+        meta_dict = create_metadata_dict_for_dataset(dataset_name=cur_service.title,
+                                                     external_metadata_dict=metadata_resource_dict)
 
-        if meta_dict['alternate_ckan_org'] is not None:
-            owner_org = meta_dict['alternate_ckan_org']
-        else:
-            owner_org = gs_datastore
+        title = cur_service.title
+        log.info('---collecting metadata for layer: {0} / {1}'.format(service_type, title))
+        owner_org = gs_datastore
+        abstract = cur_service.abstract
+        inspire_theme = 'http://inspire.ec.europa.eu/theme/ac'  # default
+        tags = None
+        if meta_dict:
+            if meta_dict['name'] is not None:
+                title = meta_dict['name']
+            if meta_dict['alternate_ckan_org'] is not None:
+                owner_org = meta_dict['alternate_ckan_org']
+            if meta_dict['notes'] is not None:
+                abstract = meta_dict['notes']
+            if meta_dict['inspire_theme_url'] is not None:
+                inspire_theme = meta_dict['inspire_theme_url']
+            if meta_dict['tag_string'] is not None:
+                tags = meta_dict['tag_string']
 
         # Create Organization with gs_datastore name in ckan if not already exists
         create_ckan_organization_if_not_exists(owner_org)
+        owner_org_url = text_unidecode.unidecode(owner_org).lower().replace(' ', '-')
 
-        abstract = cur_wfs.abstract
-        bbox = cur_wfs.boundingBox
+        bbox = cur_service.boundingBox
+
         # crs = cur_wfs.crs_list[1][4].split(':')[1]
-        crs = cur_wfs.crsOptions[0].getcode().split(':')[1]
-        log.debug("CRS Options {}".format(crs))
-        '''BBOX format ymin, xmin, ymax, xmax'''
-        bbox_ymin = bbox[0]
-        bbox_ymax = bbox[2]
-        bbox_xmin = bbox[1]
-        bbox_xmax = bbox[3]
+        if service_type == 'wfs':
+            crs = cur_service.crsOptions[0].getcode().split(':')[1]
+            bbox_crs = bbox[4].getcode().split(':')[1]
+        else:
+            crs = cur_service.crs_list[1][4].split(':')[1]
+            if bbox[4] == 'CRS:84':
+                bbox_crs = 4326
+            else:
+                bbox_crs = bbox[4].split(':')[1]
+        # log.debug("CRS Options {}".format(crs))
 
-        spatial = geojson.MultiPolygon([[[
-            crs_transformation([bbox_ymin, bbox_xmin], crs, 4326),
-            crs_transformation([bbox_ymin, bbox_xmax], crs, 4326),
-            crs_transformation([bbox_ymax, bbox_xmax], crs, 4326),
-            crs_transformation([bbox_ymax, bbox_xmin], crs, 4326),
-            crs_transformation([bbox_ymin, bbox_xmin], crs, 4326),
-        ]]])
+        spatial = make_geojson_object_from_bbox(bbox, bbox_crs)
 
-        keywords = cur_wfs.keywords
-        timepositions = cur_wfs.timepositions
+        keywords = cur_service.keywords
+        timepositions = cur_service.timepositions
 
         conforms_to = 'http://www.opengis.net/def/crs/EPSG/0/{}'.format(crs)
+
+        if cur_service == 'wcs':
+            scale_hint = cur_service.scaleHint
 
         '''Handle temporal resolution'''
         if timepositions is not None:
@@ -260,10 +161,12 @@ def create_dataset_dicts_from_wfs(wfs, metadata_resource):
             temporal_start = None
             temporal_end = None
 
-        # organization = wfs.provider.contact.organization
-        # contact_name = wfs.provider.contact.name
+        # Take organization and contact name from geoserver provider
+        # organization = service.provider.contact.organization
+        # contact_name = service.provider.contact.name
+
         contact_name = "Hydro TUD"
-        contact_email = wfs.provider.contact.email
+        contact_email = service.provider.contact.email
         # dataUrls = cur_wms.dataUrls
         # metadataUrl = cur_wms.metadataUrls
 
@@ -274,14 +177,27 @@ def create_dataset_dicts_from_wfs(wfs, metadata_resource):
              'format': 'wms',
              'licence': None,
              'description': None,
-             },
-            {'url': resource_url_wfs,
-             'name': title,
-             'format': 'wfs',
-             'licence': None,
-             'description': None,
              }
         ]
+
+        if service_type == 'wcs':
+            resource_dict.append(
+                {'url': resource_url_wcs,
+                 'name': title,
+                 'format': 'wfs',
+                 'licence': None,
+                 'description': None,
+                 }
+            )
+        else:
+            resource_dict.append(
+                {'url': resource_url_wfs,
+                 'name': title,
+                 'format': 'wfs',
+                 'licence': None,
+                 'description': None,
+                 }
+            )
         # handle url
         # Todo make this better
         ckan_dataset_url = title \
@@ -300,9 +216,9 @@ def create_dataset_dicts_from_wfs(wfs, metadata_resource):
             'contact_name': contact_name,
             'contact_uri': None,
             'url': None,
-            'tag_string': 'test',
+            'tag_string': tags,
             # 'theme': keywords,
-            'theme': 'Boden',  # hardcoded for testing
+            'theme': inspire_theme,
             'spatial': str(spatial),
             'conforms_to': conforms_to,
             'spatial_resolution': None,
@@ -313,7 +229,7 @@ def create_dataset_dicts_from_wfs(wfs, metadata_resource):
             'is_version_of': None,
             'related_resource': None,
             'derived_from': None,
-            'owner_org': owner_org,
+            'owner_org': owner_org_url,
             'license_id': None,
             'resources': resource_dict
         }
@@ -325,9 +241,9 @@ def create_dataset_dicts_from_wfs(wfs, metadata_resource):
             if v is None:
                 none_value_keys.append(k)
 
-        log.info("Following properties are passed as None-values: {}".format(none_value_keys))
+    # log.info("Following properties are passed as None-values: {}".format(none_value_keys))
 
-    log.info("Created packages from wfs sources. Ready for upload to ckan")
+    log.info("Created packages from {} sources. Ready for upload to ckan".format(service_type))
     return dataset_dicts
 
 
@@ -347,19 +263,20 @@ def create_ckan_organization_if_not_exists(org_name):
     ckan_url = config['ckan']['url']
     url = ckan_url + '/api/action/'
     header = {'Authorization': config['ckan']['apikey']}
-    organization_data = {'name': org_name.lower(),
+    org_url_str = text_unidecode.unidecode(org_name).lower().replace(' ', '-')
+    # log.debug('ORG URL = {}'.format(org_url_str))
+    organization_data = {'name': org_url_str,
                          'title': org_name}
     data_string = urllib.parse.quote(json.dumps(organization_data))
-    # data_string = urllib.parse.urlencode(dataset_dict)
     data = data_string.encode('ascii')
 
     with urllib.request.urlopen(url + 'organization_list') as response:
         contents = json.loads(response.read().decode('utf8'))
         packages = contents['result']
-        log.debug(packages)
+        # log.debug(packages)
 
-    if org_name.lower() not in packages:
-        log.info("Creating new organization with name: {}".format(org_name))
+    if org_url_str not in packages:
+        #   log.info("Creating new organization with name: {}".format(org_name))
         req = urllib.request.Request(url + 'organization_create', data, header)
         with urllib.request.urlopen(req) as response:
             the_page = response.read()
@@ -439,37 +356,18 @@ if __name__ == '__main__':
     external_metadata_resource = \
         create_metadata_dict_from_xls_file(config['external_metadata_resources']['excel'])
 
-    # datasets = create_dataset_dicts_from_wfs(wfs)
-
-    test_dict = {
-        'identifier': 'api_test2',
-        'title': 'api_test2',
-        'name': 'api_test2',
-        'contact_name': 'Simeon',
-        'spatial': '{"type":"MultiPolygon","coordinates":'
-                   '[[['
-                   '[11.75097495317459,50.557418843325934],'
-                   '[11.75097495317459,51.38755183511753],'
-                   '[13.201170265674591,51.38755183511753],'
-                   '[13.201170265674591,50.557418843325934],'
-                   '[11.75097495317459,50.557418843325934]'
-                   ']]]'
-                   '}',
-        'theme': 'Boden',
-        'owner_org': 'testing'
-    }
-    # push_dataset_to_ckan(test_dict)
-    # log.debug(datasets[2])
-
-    # log.info("Pushing all wms into ckan")
-    # create_ckan_organization_if_not_exists('organization_name')
-
-    wcs_datasets = create_dataset_dicts_from_wcs_wms(wms, wcs, external_metadata_resource)
+    wcs_datasets = create_dataset_dicts_from_wfs_or_wcs(wms=wms,
+                                                        wfs_or_wcs=wcs,
+                                                        metadata_resource_dict=external_metadata_resource,
+                                                        service_type='wcs')
     log.info("Uploading wcs/wms resources to CKAN")
     for dataset in wcs_datasets:
         push_dataset_to_ckan(dataset)
 
-    wfs_datasets = create_dataset_dicts_from_wfs(wfs, external_metadata_resource)
+    wfs_datasets = create_dataset_dicts_from_wfs_or_wcs(wms=wms,
+                                                        wfs_or_wcs=wfs,
+                                                        metadata_resource_dict=external_metadata_resource,
+                                                        service_type='wfs')
     log.info("Uploading wfs/wms resources to CKAN")
     for dataset in wfs_datasets:
         push_dataset_to_ckan(dataset)
